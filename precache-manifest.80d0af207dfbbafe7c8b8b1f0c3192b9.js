a94bccd14420dcfef73526eba00e69df73a9fe02
mohit0201 <31921322+mohit0201@users.noreply.github.com>
2019-04-03 03:15:10 +0530

    Updates

---
+++ precache-manifest.80d0af207dfbbafe7c8b8b1f0c3192b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e145d48afaf8a39af70",
+    "url": "/simple-checklist/static/css/main.65794b83.chunk.css"
+  },
+  {
+    "revision": "2e145d48afaf8a39af70",
+    "url": "/simple-checklist/static/js/main.598c8de0.chunk.js"
+  },
+  {
+    "revision": "b363c44fbf0f3b1b0208",
+    "url": "/simple-checklist/static/js/runtime~main.144a4ee1.js"
+  },
+  {
+    "revision": "0fe0e071c10a945b0328",
+    "url": "/simple-checklist/static/js/2.f4326ab3.chunk.js"
+  },
+  {
+    "revision": "0a8baac105dc02ec7c53493e8d42fc64",
+    "url": "/simple-checklist/index.html"
+  }
+];
