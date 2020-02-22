4eb3131a8b788058322b682abe6a35f1fe04f9fb
Mohit <mohityd10@gmail.com>
2020-02-23 02:14:44 +0530

    Code fixes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,10 @@ class App extends Component {
     this.setState(prevState => {
       const updatedTodos = prevState.todos.map(todo => {
         if(todo.id === id) {
-          todo.completed = !todo.completed
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
         }
         return todo
       })
@@ -26,12 +29,12 @@ class App extends Component {
       }
     })
   }
-  
+
   render() {
 
     const todoItems = this.state.todos.map(item => <ToDoItem key={item.id} item={item} handleChange={this.handleChange}/>)
 
-    return( 
+    return(
       <div className="to-do-container">
         <Header />
         <h1 className="heading">Here's your To Do List</h1>
